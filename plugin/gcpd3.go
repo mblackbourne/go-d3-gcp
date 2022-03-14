d41d5ec4fc11b687c9873db141b4ef888bc7ab0d
Maximus <maximusblackbourne@gmail.com>
2022-03-14 03:59:20 -0700

    initial Commit

---
+++ plugin/gcpd3.go
@@ -0,0 +1,32 @@
+package plugin
+
+import (
+	"github.com/ipfs/go-ipfs/plugin"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+)
+
+var Plugins = []plugin.Plugin{
+	&GCPPlugin{},
+}
+
+type GCPPlugin struct{}
+
+func (gsUtilDs GCPPlugin) Name() string {
+	return "GSUTIL-datastore-plugin"
+}
+
+func (gsUtilDs GCPPlugin) Version() string {
+	return "0.0.1"
+}
+
+func (gsUtilDs GCPPlugin) Init(env *plugin.Environment) error {
+	return nil
+}
+
+func (gsUtilDs GCPPlugin) DatastoreTypeName() string {
+	return "gsUtilDs"
+}
+
+func (gsUtilDs GCPPlugin) DatastoreConfigParser() fsrepo.ConfigFromMap {
+
+}
---
+++ plugin/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	plugin "github.com/mblackbourne/go-d3-gcp"
+)
+
+var Plugins = plugin.Plugins
+
+func main() {
+	panic("this is a plugin, build it as a plugin.")
+}
